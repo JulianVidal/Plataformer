08a1444395a0b820659bdae3d2ab9d6b6549fcd1
spiderpiggy03 <49546430+spiderpiggy03@users.noreply.github.com>
2019-08-25 13:26:39 +0300

    first commit

---
+++ Assets/scripts/PlayerTracking.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTracking : MonoBehaviour
+{
+   //Holds the position of the player
+   public Transform player;
+
+   private void FixedUpdate()
+   {
+      
+      // Moves the position of the camera to the one of the player
+      // only on the x-axis
+      gameObject.transform.position = new Vector3(player.position.x, 0, -10);
+      
+   }
+}
