e589c264f0a444d5cc6b4b7765e408a2c0fd5cbe
spiderpiggy03 <49546430+spiderpiggy03@users.noreply.github.com>
2019-10-19 19:56:47 -0400

    Jump

--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -1,140 +1,81 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor;
+/// <summary>
+/// Next I added how to jump, this became quite tricky. I still haven't come up
+/// with a solution but I got something that works mostly. Jumping is an upward force
+/// similar to the left and right forces from moving except the player should only jump if
+/// the player is touching the ground, otherwise it will jump infinelty. Trying to figure
+/// out a way to know if the player is touching the ground, when there are different height the player
+/// can be in became quite difficult. For now I settled on, if the player is touching something, it can jump.
+/// </summary>
+
+using System;
 using UnityEngine;
-using UnityEngine.EventSystems;
-using UnityEngineInternal;
 
-public class PlayerMovement : MonoBehaviour
+namespace scripts
 {
-    private bool _canJump = false; // Jump when touching the ground.
-    private bool _standUp = true; // Stand up when there is no object on top of the player
-
-    /// <summary>
-    /// These variables are used to check if the player
-    /// is touching the ground or if the player has a roof
-    /// of top of the head.
-    /// 
-    /// Corners will shoot rays up or down to find collisions
-    /// </summary>
-    //Values of the different corners of the player hit box
-    private Vector2 _bottomLeft;
-    private Vector2 _bottomRight;
-    
-    private Vector2 _topLeft;
-    private Vector2 _topRight;
-    
-    //Direction of the ray (1 up, -1 down)
-    private Vector2 _bottomDir = new Vector2(0, -1);
-    private Vector2 _topDir = new Vector2(0, 1);
-    
-    private float _rayOriginOffset = 0.1f; // Offset stops the ray from colliding with the player
-    private float _rayDistance = 0.0001f;  // How far the ray travels
-
-    
-    //Forces that changes the movement of the player
-    public float jumpForce = 200f;
-    public float walkForce = 1000f;
-    
-    // Start is called before the first frame update
-    void Start()
+    public class PlayerMovement : MonoBehaviour
     {
 
-    }
+        private Rigidbody2D _rb;
+        private Vector3 _position;
 
-    // Update is called once per frame
-    void Update()
-    {
+        private bool _canJump = true; // Jump when touching the ground.
 
-        // Checks if the player is pressing left or right key
-        // Applies a walkForce to the respective direction
-        if (Input.GetKey("right"))
+
+        //Forces that changes the movement of the player
+        public float jumpForce = 200f;
+        public float walkForce = 1000f;
+
+
+        // Start is called before the first frame update
+        private void Start()
         {
+            _rb = GetComponent<Rigidbody2D>();
+            _position = _rb.transform.position;
 
-            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(walkForce * Time.deltaTime, 0f));
-
-        } else if (Input.GetKey("left"))
-        {            
-
-            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(-walkForce * Time.deltaTime, 0f));
-
-            
-        }
-        
-        //Calculates the corners of the hit box
-        _bottomLeft  = new Vector2(gameObject.transform.position.x - 0.5f + _rayOriginOffset, gameObject.transform.position.y - 1 - _rayOriginOffset);
-        _bottomRight = new Vector2(gameObject.transform.position.x + 0.5f - _rayOriginOffset, gameObject.transform.position.y - 1 - _rayOriginOffset);
-
-        _topLeft  = new Vector2(gameObject.transform.position.x - 0.5f + _rayOriginOffset, gameObject.transform.position.y + _rayOriginOffset);
-        _topRight = new Vector2(gameObject.transform.position.x + 0.5f - _rayOriginOffset, gameObject.transform.position.y + _rayOriginOffset);
-        
-        // Casts ray and returns collisions
-        RaycastHit2D hitBottomLeft  = CastRay(_bottomLeft, _bottomDir);
-        RaycastHit2D hitBottomRight = CastRay(_bottomRight, _bottomDir);
-
-        RaycastHit2D hitTopLeft  = CastRay(_topLeft, _topDir);
-        RaycastHit2D hitTopRight = CastRay(_topRight, _topDir);
-        
-        
-        // Lets the player jump if there is a collision with the ground
-        if (hitBottomLeft.collider != null || hitBottomRight.collider != null )
-        {
-            _canJump = true;
-        }
-        
-        // Lets the player stand up if there are no collision on top
-        if (hitTopLeft.collider != null || hitTopRight.collider != null )
-        {
-
-            _standUp = false;
-        }
-        else
-        {
-            _standUp = true;
         }
 
-            
-        // Checks if the player presses the up arrow
-        //  and if the player can jump
-        //  jumps with jumpForce of force upward
-        if (Input.GetKeyDown("up") && _canJump)
-        {
-            _canJump = false;
-            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce));
-        }
-        
-        // Checks if the player is pressing the down key
-        // It will disable the long hit box and enable the smaller one
-        // Will be reverted if the player stop pressing down key and
-        // there are no collision of top
-        if (Input.GetKey("down"))
-        {
-            gameObject.GetComponent<CapsuleCollider2D>().enabled = false;
-            gameObject.GetComponent<CircleCollider2D>().enabled = true;
+        // Update is called once per frame
+        private void Update() {
+
+            //Basic movement of the player
+            Move();
+
+            _position = _rb.transform.position;
+
+            if (_canJump && Input.GetKey("space")) {
+              _rb.AddForce(new Vector2(0, jumpForce * Time.deltaTime));
+
+            }
 
         }
-        else if (_standUp)
+
+
+        private void OnTriggerEnter2D(Collider2D other)
         {
-            gameObject.GetComponent<CapsuleCollider2D>().enabled = true;
-            gameObject.GetComponent<CircleCollider2D>().enabled = false;
+            if (other.gameObject.CompareTag("Floor"))
+            {
+                _canJump = true;
+            }
+        }
+
+
+        private void Move() {
+            // Checks if the player is pressing left or right key
+          // Applies a walkForce to the respective direction
+          if (Input.GetKey("right"))
+          {
+              _rb.AddForce(new Vector2(walkForce * Time.deltaTime, 0));
+              _playerDir = 1;
+
+          }
+          else if (Input.GetKey("left"))
+          {
+              _rb.AddForce(new Vector2(-walkForce * Time.deltaTime, 0));
+              _playerDir = 0;
+          }
 
         }
-        
+
 
     }
-
-    // Shoots a ray from an origins, towards a direction with the
-    // distance of _rayDistance and returns collisions 
-    // otherwise null
-    private RaycastHit2D CastRay(Vector2 origin, Vector2 direction)
-    {
-
-        Debug.DrawRay(origin, direction, Color.red, 0f, false);
-       
-        return Physics2D.Raycast(origin, direction, _rayDistance);
-        
-    }
-    
 }
- 
